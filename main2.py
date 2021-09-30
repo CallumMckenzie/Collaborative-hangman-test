42aaa14f123fcdd08fe1af29939207936cfa0f6b
crispygummy <callum.hugh.mckenzie@gmail.com>
2021-09-30 17:32:15 +1000

    bug fixes + improvements
    I have added a feature that allows part of the answer to be entered,
    recorded and then returned with the other parts of the answer. I fixed
    the bug that was causing only the first word in the file to be answered

--- main2.py
+++ main2.py
@@ -8,20 +8,35 @@ def Play_mode(play, difficulty):
     guesses = 0
     lives_options = [10, 6, 3, 10]
     lives = lives_options[difficulty]
+    unfinished_answer = []
     # draw a word from a text file
-    with open ('words.txt') as word_file:
-        word_list = word_file.readline()
-
+    with open('words.txt') as word_file:
+        words = word_file.readline()
+        word_list = words.split(',')
+        rand_index = random.randint(0, len(word_list) - 1)
+        answer = word_list[rand_index]
+        for letters in answer: unfinished_answer.append('_')
     while play is True and lives >= 1:
         guess = input("Please enter a single alphabetical character")
         if guess.isalpha() is False:
             print("Error, you must enter a alphabetical character")
+        # this handles full word guesses
         elif len(guess) > 1:
             if guess == answer:
-                print('You guessed the correct word, you win! \n your stats are lives = ' + lives + ',\
-                 guesses = ' + guesses)
+                print('You guessed the correct word, you win! \n your stats are lives = ' + str(lives) + ',\
+                 guesses = ' + str(guesses))
             else:
                 guesses += 1
+                lives -= 1
+            if difficulty == 'test': print(answer)
+        else:
+            if guess in answer:
+                #this will add the guess to the unfinished answer
+                unfinished_answer[answer.index(guess)] = guess
+                print(unfinished_answer)
+            else:
+                guesses += 1
+                lives -= 1
 # test comment
 def menu():
     """ This function will allow the user to decide if they want to
@@ -35,18 +50,20 @@ def menu():
                 # add a lives choice here later
                 dif_menu_done = False
                 while dif_menu_done is False:
+                    # maybe use a list instead of a cascade
                     difficulty_check = input("what difficulty would you like to play on? easy, medium, hard or test?")
                     if difficulty_check == "easy":
-                        difficulty = 1
+                        difficulty = 0
                         dif_menu_done = True
                     elif difficulty_check == "medium":
                         difficulty = 1
                         dif_menu_done = True
                     elif difficulty_check == "hard":
-                        difficulty = 3
+                        difficulty = 2
                         dif_menu_done = True
                     elif difficulty_check == "test":
-                        difficulty = 4
+                        difficulty = 3
+                        dif_menu_done = True
                     else:
                         print("Please select one of the available options")
                 play = True
